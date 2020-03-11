79652639d729533e503d65514c1bdeff225d5703
RubelHasan <rubelhasan306@gmail.com>
2020-03-12 00:32:07 +0600

    Components and JSX

--- src/App.js
+++ src/App.js
@@ -3,21 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const products = [
+    {name: 'Photoshop', price: '$230'},
+    {name: 'Illustartor', price: '$150'},
+    {name: 'After Effect', price: '$450'},
+  ]
+
+
+  function PersonName(props){
+    const personNameStyle = {
+      backgroundColor:'gray', 
+      margin: '10px',
+      border: '2px solid red',
+      padding: '15px'
+    }
+    return (
+      <div style={personNameStyle}>
+        <h2>{props.name}</h2>
+        <h4>{props.price}</h4>
+      </div>
+    )
+  }
+  const myProducts = [
+    {name: 'Apple', price:'$120'},
+    {name: 'Mango', price:'$160'},
+    {name: 'Orange', price:'$60'},
+    {name: 'Pinaple', price:'$20'}
+  ]
+  function ProductsAll(props){
+    const ProductsAllStyle = {
+      padding: '20px',
+      border:'2px solid yellow',
+      backgroundColor: 'salmon',
+      margin: '20px',
+      width: '300px',
+      float:'left'
+    }
+
+
+    return (
+      <div style={ProductsAllStyle}>
+          <h1>{props.products.name}</h1>
+          <h3>{props.products.price}</h3>
+      </div>
+    )
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>First React Component </h2>
+        <PersonName name={products[0].name} price= {products[0].price}></PersonName>
+        <PersonName name={products[1].name} price= {products[1].price}></PersonName>
+        <PersonName name={products[0].name} price= {products[0].price}></PersonName>
+        <ProductsAll products= {myProducts[0]}></ProductsAll>
+        <ProductsAll products= {myProducts[1]}></ProductsAll>
+        <ProductsAll products= {myProducts[2]}></ProductsAll>
+        <ProductsAll products= {myProducts[3]}></ProductsAll>
       </header>
     </div>
   );
